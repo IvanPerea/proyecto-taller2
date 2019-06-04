e209caa2666426679e10cae456240f84f1aa07f1
Ivan Rafael Perea Ostos <ivan.perea@usil.pe>
2019-06-04 10:19:26 -0500

    versiones

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+    $to = 'ivan.perea@usil.pe';
+    $firstname = $_POST["fname"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    $phone= $_POST["phone"];
+    
+
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'  '.$laststname.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>numero: '.$phone.'</td></tr>
+        <tr><td>Texto: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'El mensaje ha sido enviado correctamente.';
+    }else{
+        echo 'failed';
+    }
+
+?>
